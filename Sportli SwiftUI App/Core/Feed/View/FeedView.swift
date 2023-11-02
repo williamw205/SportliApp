ea0e29fb58b91f5623b83f8073b27a30d613c4eb
William Wong <williamw205@gmail.com>
2023-11-02 17:11:13 -0500

    Organizing files

---
+++ Sportli SwiftUI App/Core/Feed/View/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  Sportli SwiftUI App
+//
+//  Created by William Wong on 11/2/23.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
