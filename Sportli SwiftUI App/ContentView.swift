0e2829d4d64e01cf23b9d9e40e195b373f6ad2b3
William Wong <williamw205@gmail.com>
2023-11-02 10:33:06 -0500

    Initial Commit

---
+++ Sportli SwiftUI App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sportli SwiftUI App
+//
+//  Created by William Wong on 11/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sportli SwiftUI App/Sportli_SwiftUI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sportli_SwiftUI_AppApp.swift
+//  Sportli SwiftUI App
+//
+//  Created by William Wong on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Sportli_SwiftUI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
