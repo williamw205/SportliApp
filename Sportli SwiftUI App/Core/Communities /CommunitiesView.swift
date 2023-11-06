fc8eafe64f2bb9c40e906aaf59bd515e698d86b5
William Wong <williamw205@gmail.com>
2023-11-06 08:34:58 -0600

    Working on communities message view horizontal scrollview

---
+++ Sportli SwiftUI App/Core/Communities /CommunitiesView.swift
@@ -0,0 +1,18 @@
+//
+//  CommunitiesView.swift
+//  Sportli SwiftUI App
+//
+//  Created by William Wong on 11/6/23.
+//
+
+import SwiftUI
+
+struct CommunitiesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommunitiesView()
+}
